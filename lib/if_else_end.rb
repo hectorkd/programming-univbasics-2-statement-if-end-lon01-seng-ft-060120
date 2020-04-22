be76ad45b9a56b251c7c1157b2bc9b66bc702ed5
Hector Kennedy-Dyson <hectorekd@gmail.com>
2020-04-22 13:26:55 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,3 +5,12 @@ if run_code_inside
   puts "code inside"
 end 
 puts "Code before if...end"
+
+chance_of_rain = 0.2 
+if chance_of_rain <= 0.25
+  puts "pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+puts "pack an umberella"
+else 
+  puts "stay at home"
+end
